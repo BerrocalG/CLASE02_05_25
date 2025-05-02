202d8b7dbdb0a20dfa5285b30c04163112f5aa70
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-02 11:47:05 -0500

    adc

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,28 @@
 #include <util/delay.h>
 
 
-
 int main(void) {
-    DDRD|=0X0C; 
-    DDRD|=0XF0; 
+    DDRD|=0XFC; 
     DDRB|=0X0F; 
+    ADMUX|=(1<<REFS0)|(1<<MUX1);
+    ADCSRA|=(1<<ADEN)|(1<<ADPS2);
+    char alow=0;
+    char ahigh=0;
 
     while(1){
-        PORTB|=0X0F;
-        PORTD|=0XFC;
-        _delay_ms(500);
+        ADCSRA|=(1<<ADSC);
+        while(!(ADCSRA&(1<<ADIF)));//conversion del adc
+        alow=ADCL;//primero siempre el adcl par aque funcione bien
+        ahigh=ADCH;
+
+        //para terminar debo de valer a 0 la bandera
+        ADCSRA|=(1<<ADIF);
+        //PARA QUE EMPIECEN APAGADOS
         PORTB&=~0X0F;
         PORTD&=~0XFC;
-        _delay_ms(500);
+        PORTD|=(alow<<2);
+        PORTB|=(ahigh&0x03)<<2 | (alow>>6);
+        
+        
     }
 }
