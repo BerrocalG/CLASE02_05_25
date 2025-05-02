f590450143db32dc2850c3b67dbaf34c6c5e32d4
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-02 11:24:55 -0500

    PRENDER APAGAR LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,22 @@
 //clase ADC
+//EJERCICIO 1 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+int main(void) {
+    DDRD|=0X0C; 
+    DDRD|=0XF0; 
+    DDRB|=0X0F; 
+
+    while(1){
+        PORTB|=0X0F;
+        PORTD|=0XFC;
+        _delay_ms(500);
+        PORTB&=~0X0F;
+        PORTD&=~0XFC;
+        _delay_ms(500);
+    }
+}
