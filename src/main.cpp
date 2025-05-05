38b3fb6fe0111abf6899e24eacf09f5e48816515
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-04 19:36:54 -0500

    2 ejercicios clase

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,36 @@
+//ejercicio 1
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void){
+
+DDRD|=0XFC; 
+DDRB|=0X0F; 
+ADMUX|=(1<<REFS0)|(1<<MUX1);
+ADCSRA|=(1<<ADEN)|(1<<ADPS2);
+char alow=0;
+char ahigh=0;
+
+while(1){
+    ADCSRA|=(1<<ADSC);
+    while (!(ADCSRA&(1<<ADIF))); 
+    alow=ADCL;//primero siempre el adcl par aque funcione bien
+    ahigh=ADCH;
+
+    //para terminar debo de valer a 0 la bandera
+    ADCSRA|=(1<<ADIF);
+    //PARA QUE EMPIECEN APAGADOS
+    PORTB&=~0X0F;
+    PORTD&=~0XFC;
+    PORTD|=(alow<<2);
+    PORTB|=((ahigh&0x03)<<2) |( (alow>>6)&0x03);
+}
+}
+
+/*
 //clase ADC
-//EJERCICIO 1 
+//EJERCICIO 2 
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
@@ -29,3 +60,6 @@ int main(void) {
         PORTD|=((ahigh<<4) &(0xF0))|((alow>>4)&(0x0C));
     }
 }
+
+
+*/
