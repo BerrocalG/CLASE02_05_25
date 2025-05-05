80243f26972905b7a87bac9749d8ff02b2a53b2f
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-05 13:10:10 -0500

    comprension

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,27 @@ int main(void){
 DDRD|=0XFC; 
 DDRB|=0X0F; 
 ADMUX|=(1<<REFS0)|(1<<MUX1);
+//ref referenciaa los 5v
+//mux1 adc2 como analoga
 ADCSRA|=(1<<ADEN)|(1<<ADPS2);
+//aden habilita el adc
+//desplaza bit a la posicion c2
 char alow=0;
 char ahigh=0;
 
 while(1){
     ADCSRA|=(1<<ADSC);
     while (!(ADCSRA&(1<<ADIF))); 
+    //adsc para iniciar conversion analogica
+    //adif es la bandera
     alow=ADCL;//primero siempre el adcl par aque funcione bien
+    //bit bajo 8 bits (0-7)  
     ahigh=ADCH;
+    //bit alto 2 bits (8-9)
 
     //para terminar debo de valer a 0 la bandera
     ADCSRA|=(1<<ADIF);
+
     //PARA QUE EMPIECEN APAGADOS
     PORTB&=~0X0F;
     PORTD&=~0XFC;
